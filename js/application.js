0eef1baa42eaaa005b310c1b6ee4725cb7e2b86a
jrojas <jorge.l.rojas@gmail.com>
2013-12-09 17:06:17 -0800

    prompt
    using the prompt function to test the basic function of application.

--- js/application.js
+++ js/application.js
@@ -1,10 +1,16 @@
 // JavaScript Document
+var number = Math.floor(Math.random()*100);
+var guessNumber = (prompt("Pick a number", ""));	
 
 function getRandomNumber()
 {
-var number = Math.floor(Math.random()*100);
-		console.log(number);
-		$(".message").html(number);
 		
-		
+	if(guessNumber == number)
+	alert ("you guessed the right number") + $(".number").html(guessNumber + ' ' + "Is the right Number");	
+	else
+	alert ("you guessed the wrong number" ) + $(".number").html(guessNumber + ' ' + "Is the not" + ' ' + number);	
+	
 }
+
+console.log(guessNumber);	
+console.log(number);
